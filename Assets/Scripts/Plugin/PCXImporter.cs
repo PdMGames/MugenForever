3f51f79001d56b136b89b4a2f85b03f7d48a3c23
Eduardo Bonfandini <trialforce@gmail.com>
2023-05-06 21:30:36 -0300

    Adicionando suporte a PCX como asset de textura nativo

---
+++ Assets/Scripts/Plugin/PCXImporter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor.AssetImporters;
+using System.IO;
+using MugenForever.IO.PCX;
+using UnityEditor;
+
+[ScriptedImporter(1,"pcx")]
+public class PcxImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        Debug.Log(ctx.assetPath);
+
+        FileStream fileStream = new FileStream(ctx.assetPath, FileMode.Open, FileAccess.Read);
+        fileStream.Seek(0, SeekOrigin.Begin);
+
+        IPCXImage readPCXImage = new PCXImageImpl(fileStream);
+        Texture2D texture = readPCXImage.Texture2D;
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), 100f);
+
+        ctx.AddObjectToAsset("sprite", sprite);
+        ctx.AddObjectToAsset("main", texture);
+        ctx.SetMainObject(texture);
+    }
+}
